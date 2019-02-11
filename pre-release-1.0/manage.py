a8d9d74cf4e365bf5a1c9ab31f76924763b8541b
AmericanCommunist <zeyuanjiang@gmail.com>
2019-02-11 12:29:40 -0800

    manage error

--- pre-release-1.0/manage.py
+++ pre-release-1.0/manage.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env python
 import os
 import sys
@@ -14,22 +13,3 @@ if __name__ == "__main__":
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-=======
-#!/usr/bin/env python
-import os
-import sys
-
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cruzmail.settings")
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
->>>>>>> 1444814b112073d79f6ea4eebbf9bb02dbde35fa
