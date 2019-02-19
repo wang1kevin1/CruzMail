e72273631394e8a6881fee14aa63fee0ccfaaf51
sshingane <sshingan@ucsc.edu>
2019-02-19 15:44:28 -0800

    setup database and user verification

---
+++ pre-release-1.0/cruzmail/collection/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+from django.contrib.auth import views as auth_views
+from .views import *
+
+urlpatterns = [
+	url(r'^$', views.collection, name='collection'),
+	#url(r'^users', views.users, name='users'),
+
+]
