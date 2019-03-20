6d8028f0cb89c57e3e49d20c3253dc59bfad33af
Kevin Wang <wang1kevin1@gmail.com>
2019-03-19 17:42:22 -0700

    added documentation

--- pre-release-1.0/cruzmail/settings.py
+++ pre-release-1.0/cruzmail/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cruzmail_db',
         'USER': 'root',
-        'PASSWORD' : 'hihihihi1',
+        'PASSWORD' : 'changeme',
         'HOST' : '127.0.0.1',
         'PORT' : '3306',
     }
